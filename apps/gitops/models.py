b4dcbb134d1700fc3bd53427cf673444e392f174
Timothy <timothy@hobbs.cz>
2024-03-01 13:41:12 +0000

    Update models.py

--- apps/gitops/models.py
+++ apps/gitops/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Gitops(models.Model):
     id = models.UUIDField(default=uuid4, editable=False, unique=True, primary_key=True)
     secret_key = models.CharField(max_length=100, blank=False, null=False)
-    name = models.CharField(max_length=100, blank=False, null=False)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
     keycloak_group_id = models.CharField(max_length=100, null=False, blank=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
